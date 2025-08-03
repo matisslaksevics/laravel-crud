75cb01f2f20f8bfe37b09751be3883fd06f3d227
Matiss Laksevics <matiss.laksevics12@gmail.com>
2025-08-03 15:40:46 +0300

    added registration logic with mysql connection

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function register() {
+    public function register(Request $request) {
+        $requestedInfo = $request->validate([
+            'name' => ['required', 'min:3', 'max:10'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8', 'max:200']
+        ]);
+
+        $requestedInfo['password'] = bcrypt($requestedInfo['password']);
+        User::create($requestedInfo);
+
         return 'Hello from controller!';
     }
 }
